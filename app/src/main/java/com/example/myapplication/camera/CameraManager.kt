e14a4bbf8adbd18e8320d9992a3455fe15322fd6
Paradorn-248 <paradorn248@gmail.com>
2023-02-25 22:31:16 +0700

    add face detection

---
+++ app/src/main/java/com/example/myapplication/camera/CameraManager.kt
@@ -0,0 +1,91 @@
+package com.example.myapplication.camera
+
+import android.content.Context
+import android.util.Log
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import com.example.myapplication.face_detection.FaceContourDetectionProcessor
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+@ExperimentalGetImage class CameraManager(
+    private val context: Context,
+    private val finderView: PreviewView,
+    private val lifecycleOwner: LifecycleOwner,
+    private val graphicOverlay: GraphicOverlay
+) {
+
+    private var preview: Preview? = null
+
+    private var camera: Camera? = null
+    private lateinit var cameraExecutor: ExecutorService
+    private var cameraSelectorOption = CameraSelector.LENS_FACING_FRONT
+    private var cameraProvider: ProcessCameraProvider? = null
+
+    private var imageAnalyzer: ImageAnalysis? = null
+
+    init {
+        createNewExecutor()
+    }
+
+    private fun createNewExecutor() {
+        cameraExecutor = Executors.newSingleThreadExecutor()
+    }
+
+    fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
+        cameraProviderFuture.addListener(
+            {
+                cameraProvider = cameraProviderFuture.get()
+                preview = Preview.Builder()
+                    .build()
+
+                imageAnalyzer = ImageAnalysis.Builder()
+                    .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                    .build()
+                    .also {
+                        it.setAnalyzer(cameraExecutor, selectAnalyzer())
+                    }
+
+                val cameraSelector = CameraSelector.Builder()
+                    .requireLensFacing(cameraSelectorOption)
+                    .build()
+
+                setCameraConfig(cameraProvider, cameraSelector)
+
+            }, ContextCompat.getMainExecutor(context)
+        )
+    }
+
+    private fun selectAnalyzer(): ImageAnalysis.Analyzer {
+        return FaceContourDetectionProcessor(graphicOverlay)
+    }
+
+    private fun setCameraConfig(
+        cameraProvider: ProcessCameraProvider?,
+        cameraSelector: CameraSelector
+    ) {
+        try {
+            cameraProvider?.unbindAll()
+            camera = cameraProvider?.bindToLifecycle(
+                lifecycleOwner,
+                cameraSelector,
+                preview,
+                imageAnalyzer
+            )
+            preview?.setSurfaceProvider(
+                finderView.surfaceProvider
+            )
+        } catch (e: Exception) {
+            Log.e(TAG, "Use case binding failed", e)
+        }
+    }
+
+    companion object {
+        private const val TAG = "CameraXBasic"
+    }
+
+}
