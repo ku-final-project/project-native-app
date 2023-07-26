19e23660865bbf60a9ae203ad5d5416b02d949ae
tawanchaiii <tawanchai_champ@hotmail.com>
2023-07-26 17:02:14 +0700

    Change login method to used token (with mock API)

--- app/src/main/java/com/example/myapplication/FaceDetectionPage.kt
+++ app/src/main/java/com/example/myapplication/FaceDetectionPage.kt
@@ -2,28 +2,50 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.ExperimentalGetImage
 import com.blogspot.atifsoftwares.animatoolib.Animatoo
 import kotlinx.android.synthetic.main.face_detection.*
+import com.example.myapplication.api.ApiService
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.json.JSONObject
 
 @ExperimentalGetImage class MainActivity2 : AppCompatActivity() {
+    private lateinit var apiService: ApiService
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.face_detection)
+        createApiService()
         // hide actionBar and statusBar
         supportActionBar?.hide()
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         login_button.setOnClickListener{
-            if(username.text.toString()=="1" && password.text.toString()=="1"){
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-                Animatoo.animateSlideLeft(this)
-            }else{
-                Toast.makeText(applicationContext, "Invalid username or password", Toast.LENGTH_SHORT).show()
+            val scope = CoroutineScope(Dispatchers.IO + CoroutineName("MyScope"))
+            var response: JSONObject? = null;
+            scope.launch(Dispatchers.IO) {
+                response = apiService.authTerminal(JSONObject(
+                    mapOf("baseUrlAPI" to baseurlapi.text.toString(),
+                        "eventId" to eventid.text.toString(),
+                        "token" to token.text.toString() )
+                ))
+                Log.i("Response", response.toString())
+                if (response != null) {
+                    val intent = Intent(this@MainActivity2, MainActivity::class.java)
+                    startActivity(intent)
+                    Animatoo.animateSlideLeft(this@MainActivity2)
+                }else{
+                    Toast.makeText(applicationContext, "response is null", Toast.LENGTH_SHORT).show()
+                }
             }
         }
+    }
+    private fun createApiService(){
+        apiService = ApiService()
     }
 }
--- app/src/main/java/com/example/myapplication/api/ApiService.kt
+++ app/src/main/java/com/example/myapplication/api/ApiService.kt
@@ -4,18 +4,19 @@ import android.util.Log
 import com.goebl.david.Webb
 import org.json.JSONObject
 
-class ApiService{
-    fun webbGetHello(){
+class ApiService {
+    fun webbGetHello() {
         val web = Webb.create()
-        val result = web.get("http://tawanchaiserver.ddns.net:8001/")
+        val result = web.get("https://mock-ku-api.tawanchai-champ.workers.dev/hello")
             .ensureSuccess()
             .asJsonObject()
             .body
         Log.i("Webb API", result.toString())
     }
-    fun webbPostImage(pic:String, face_id:String): JSONObject? {
+
+    fun webbPostImage(pic: String, face_id: String): JSONObject? {
         val web = Webb.create()
-        val result = web.post("https://apiface.tawanchai.com/upload")
+        val result = web.post("https://mock-ku-api.tawanchai-champ.workers.dev/upload")
             .header("Content-Type", "application/json")
             .body(
                 JSONObject(
@@ -27,5 +28,17 @@ class ApiService{
             .body
         Log.i("Webb API", result.toString())
         return result
+    }
+
+    fun authTerminal(payload: JSONObject): JSONObject? {
+        val httpClient = Webb.create()
+        return httpClient.post("https://mock-ku-api.tawanchai-champ.workers.dev/login")
+            .header("Content-Type", "application/json")
+            .body(
+                payload.toString()
+            )
+            .ensureSuccess()
+            .asJsonObject()
+            .body
     }
 }
