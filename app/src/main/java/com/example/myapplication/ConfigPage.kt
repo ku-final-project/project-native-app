8db23aeebb17c11668478848adc3b2feaf1a8452
Paradorn-248 <paradorn248@gmail.com>
2023-10-13 14:19:44 +0700

    add some config

--- app/src/main/java/com/example/myapplication/ConfigPage.kt
+++ app/src/main/java/com/example/myapplication/ConfigPage.kt
@@ -1,8 +1,11 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.provider.Settings.Global
+import android.text.Editable
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.config_page.apiUrlInput
 
 class ConfigPage : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/myapplication/LoginPage.kt
+++ app/src/main/java/com/example/myapplication/LoginPage.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings.Global
 import android.util.Log
 import android.view.View
 import android.widget.Toast
