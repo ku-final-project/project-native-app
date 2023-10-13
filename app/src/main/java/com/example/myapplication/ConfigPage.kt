95e9b84537b5b0b85873e4d34dcb25dd1f0b95b7
Paradorn-248 <paradorn248@gmail.com>
2023-10-13 14:32:00 +0700

    add save button

--- app/src/main/java/com/example/myapplication/ConfigPage.kt
+++ app/src/main/java/com/example/myapplication/ConfigPage.kt
@@ -1,18 +1,28 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.provider.Settings.Global
-import android.text.Editable
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.config_page.apiUrlInput
+import com.example.myapplication.api.ApiService
+import kotlinx.android.synthetic.main.config_page.save_config_button
 
 class ConfigPage : AppCompatActivity(){
+    // API Service
+    private lateinit var apiService: ApiService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.config_page)
         // hide actionBar and statusBar
         supportActionBar?.hide()
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        save_config_button.setOnClickListener{
+//            apiService.
+        }
+    }
+
+    private fun createApiService(){
+        apiService = ApiService()
     }
 }
