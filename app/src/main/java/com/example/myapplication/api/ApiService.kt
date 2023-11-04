f0d325803ec828e48373debfd097c4051db1b69c
Paradorn-248 <paradorn248@gmail.com>
2023-11-04 16:26:22 +0700

    finish

--- app/src/main/java/com/example/myapplication/api/ApiService.kt
+++ app/src/main/java/com/example/myapplication/api/ApiService.kt
@@ -1,10 +1,13 @@
 package com.example.myapplication.api
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.goebl.david.Webb
 import org.json.JSONObject
 
-class ApiService{
+class ApiService(private val context: Context){
     fun webbGetHello(){
         val web = Webb.create()
         val result = web.get("http://tawanchaiserver.ddns.net:8001/")
@@ -14,8 +17,15 @@ class ApiService{
         Log.i("Webb API", result.toString())
     }
     fun webbPostImage(pic:String, face_id:String): JSONObject? {
+        var config: SharedPreferences? = context.getSharedPreferences("config", AppCompatActivity.MODE_PRIVATE)
+        var apiUrl = config?.getString("API_URL", "")
+        var token = config?.getString("TOKEN", "")
+        var eventId = config?.getString("EVENT_ID", "")
+        Log.i("config", apiUrl!!)
+        Log.i("config", token!!)
+        Log.i("config", eventId!!)
         val web = Webb.create()
-        val result = web.post("https://mock-ku-api.tawanchai-champ.workers.dev/upload")
+        val result = web.post(apiUrl)
             .header("Content-Type", "application/json")
             .body(
                 JSONObject(
